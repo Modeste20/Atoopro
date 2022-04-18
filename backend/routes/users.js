3ac628c743338fe04c8969d9725d15ef90f4f7e5
Modeste Landry <jojomodeste52@gmail.com>
2022-04-18 15:59:09 +0200

    first commit

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,5 @@
 const {userSchema,loginUserSchema} = require('../Schema/users.schema')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const Router = require('express').Router()
 
