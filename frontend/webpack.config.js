c1905179f99ef8378095f93ae5236e1db5568902
Modeste Landry <jojomodeste52@gmail.com>
2022-04-24 15:01:18 +0200

    Multi-language fonctionnality implemented !

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.join(__dirname, 'build'),
+        path: path.join(__dirname, 'public'),
         publicPath:'/'
     },
     devServer: {
