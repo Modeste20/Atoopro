5752189b3319028753924d0997ff7bea74b6e80c
Modeste Landry <jojomodeste52@gmail.com>
2022-06-07 12:09:50 +0200

    Commit

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,6 +1,6 @@
 import { Col, Row } from 'antd'
 import React, { useContext, useRef} from 'react'
-import {ReactComponent as ImageHomeBannerPc } from './../../File/images/home-banner-pc.svg'
+import ImageHomeBannerPc from './../../File/images/home-banner-pc.png'
 import Accordeon from './Components/Accordeon/Accordeon'
 import Partenaires from './Components/Partenaires/Partenaires'
 import ServiceCard from './Components/ServiceCard/ServiceCard'
@@ -43,14 +43,12 @@ const Home = ({t}) => {
 
                             <h1 className='pb-2'>{t('banner.title','Bienvenue sur Atoo pro')}</h1>
 
-                            <p>{t('banner.paragraphe')}</p>
-
                             <p>
                                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae minima rerum nemo provident laborum veritatis, vel necessitatibus magnam ipsam omnis numquam quam totam iusto earum incidunt ipsum quasi! Deserunt, mollitia.
                             </p>
 
                             <div style={{ display: 'flex' , alignItems:'center' }}>
-                                <Link className='banner-button-contact btn-link-primary' to='/contactez-nous' style={{ marginTop: 25, height: 45, padding:'0 15px !important',borderRadius: 5 }}>{t('banner.button')}</Link>
+                                <Link className='banner-button-contact btn-link-primary' to='/contactez-nous' style={{ marginTop: 25, height: 45, padding:'0 15px !important',borderRadius: 5 }}>Contactez-nous</Link>
                                 {
                                     isAdmin && <Link style={{marginLeft:15}} to='/admin'>Admin account</Link>
                                 }
@@ -61,8 +59,8 @@ const Home = ({t}) => {
                     <Col xs={24} md={12} lg={8} xxl={8} className='banner-image'>
                         <FadeComponent right>
                             <div className='img'>
-                                {/*<img src={ImageHomeBannerPc} alt='r' width={'100%'} className='' />*/}
-                                <ImageHomeBannerPc />
+                                <img src={ImageHomeBannerPc} alt='r' width={'100%'} className='' />
+                                {/*<ImageHomeBannerPc />*/}
                             </div>
                         </FadeComponent>
 
